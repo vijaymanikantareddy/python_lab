68dd1ae996dfeb112827468f99d6437fd0a361ec
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-19 10:35:39 +0530

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -4,3 +4,12 @@ c=int(input('enter 3rd number: '))
 sum=a+b+c
 average=(a+b+c)/3
 print('sum = ',sum,'\nAverage = ',average)
+
+
+#Output:
+#enter 1st number: 5
+#enter 2nd number: 6
+#enter 3rd number: 7
+#sum =  18 
+#Average =  6.0
+
