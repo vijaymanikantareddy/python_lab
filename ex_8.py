8b1895d5040de45e6a53b75f382fc02cc35b0038
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-19 10:54:40 +0530

    Create ex_8.py

---
+++ ex_8.py
@@ -0,0 +1,28 @@
+status = False
+for i in word:
+    if i in "aeiouAEIOU":
+        status = True
+if status == True:
+    print('word contains vowels')
+else: 
+    print('word does not contains vowels')
+word=input('enter the word: ')
+status = False
+for i in word:
+    if i in "aeiouAEIOU":
+        status = True
+if status == True:
+    print('word contains vowels')
+else: 
+    print('word does not contains vowels')
+    
+    
+    
+ #Output1:
+#enter the word: why
+#word does not contains vowels
+
+
+#Output2:
+#enter the word: python
+#word contains vowels
