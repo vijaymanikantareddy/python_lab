d1e805944ab0cd71cd91bb2c9fd5439b2150b07b
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-19 10:44:51 +0530

    Create ex_7.py

---
+++ ex_7.py
@@ -0,0 +1,12 @@
+a=float(input('enter number A: '))
+b=float(input('enter number B: '))
+if abs(a-b)<=0.001:
+    print('close')
+else:
+    print("not close")
+    
+    
+#Output
+#enter number A: 5.001
+#enter number B: 5.002
+#close
