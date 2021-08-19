9f36839a9b3ea377dc0afc9ef935b55400eb85b9
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-19 10:30:28 +0530

    Create ex_4.py

---
+++ ex_4.py
@@ -0,0 +1,10 @@
+name=input('enter the name: ')
+n=int(input('enter the number of times to print: '))
+print(name*n)
+
+
+
+#Output:
+#enter the name: vijay
+#enter the number of times to print: 10
+#vijayvijayvijayvijayvijayvijayvijayvijayvijayvijay
