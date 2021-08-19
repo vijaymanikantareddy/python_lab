7fff3d3860cbd6b66e95a1ea8b7debba372ec5be
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-19 10:34:54 +0530

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,3 +1,7 @@
 kg = float(input('enter weight in kg: '))
 pounds=kg*2.2
 print('weight in kg = ',kg,'\nWeight in pounds = ',pounds)
+
+#enter weight in kg: 26
+#weight in kg =  26.0 
+#Weight in pounds =  57.2
