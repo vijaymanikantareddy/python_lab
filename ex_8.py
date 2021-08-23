030ef25e79dc7616cb81255d2bca73aef1db1e09
vijaymanikantareddy <83952736+vijaymanikantareddy@users.noreply.github.com>
2021-08-23 10:24:46 +0530

    Update ex_8.py

--- ex_8.py
+++ ex_8.py
@@ -9,6 +9,17 @@ else:
     print('word does not contains vowels')
 
     
+s=input("enter a string: ")
+v=0
+for i in s:
+    if i=='a' or i=='e' or i=='o' or i=='u' or i=='i' or i=='A' or i=='E' or i=='O' or i=='U' or i=='I':
+        v=v+1
+if v==0:
+    print('string does not contains vowels')
+else:
+    
+    print('string contains vowels.')
+    
     
     
  #Output1:
